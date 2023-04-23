54422d428ddd59991a66e38dd6ff877f3c2bb3b4
Ryan Rudd <ry.an.ruddd@gmail.com>
2023-04-23 12:57:04 -0400

    Update trainData.py

--- avs/engine/trainData.py
+++ avs/engine/trainData.py
@@ -1,13 +1,13 @@
 from ctypes import cast, py_object
 import uuid
 import os
+import re
 
 class TrainData(object):
-    def __init__(self, VIDEO_TAGS, VIDEO_PUBLISHER, VIDEO_TOPIC, VIDEO_STAMP):
+    def __init__(self, VIDEO_TAGS, VIDEO_PUBLISHER, VIDEO_TOPIC):
         self.VIDEO_TAGS = VIDEO_TAGS
         self.VIDEO_PUBLISHER = VIDEO_PUBLISHER
         self.VIDEO_TOPIC = VIDEO_TOPIC
-        self.VIDEO_STAMP = VIDEO_STAMP
 
 class ModelTrainer:
     def trainAVSdata(data: TrainData, user_id, train_data_folder = "train_data"):
@@ -20,16 +20,42 @@ class ModelTrainer:
             os.mkdir(train_data_folder)
             print("Succesfuly Create train model directory")
             pass
-        
+        if not os.path.isfile(f'{train_data_folder}/{user_id}.train_data'):
+            with open(f'{train_data_folder}/{user_id}.train_data', "w") as file:
+                FILE_DATA = """
+PUBLISHERS:[]
+TAGS:[]
+TOPICS:[]
 
+RES:{
+    SUGGESTED_PUBLISHERS:[]
+    SUGGESTED_TAGS:[]
+    SUGGESTED_TOPICS:[]
+}
+                """
+                file.write(FILE_DATA)
+                pass
 
-        
-        
+        with open(f'{train_data_folder}/{user_id}.train_data', 'r') as file_data:
+
+            file_data_text = str(file_data.read())
+
+            TARGET_PUBLISHERS_ARRAY = "PUBLISHERS:"
+            TARGET_TAGS_ARRAY = "TAGS:"
+            TARGET_TOPICS_ARRAY = "TOPICS:"
+
+            ARRAY_DEFINITION_BEGIN_TOKEN = "["
+            ARRAY_DEFINITION_END_TOKEN = "]"
+            ARRAY_CONTINOUS_ITEM_TOKEN = ","
+
+            # SEARCH FOR PUBLISHERS ARRAY INDEX
+            publisher_res = re.search(TARGET_PUBLISHERS_ARRAY, file_data_text)
+            print(f"Found publisher array index from training model: {publisher_res.span()}")
+
 
 user_data = TrainData(
     VIDEO_PUBLISHER="PUBLISHER",
     VIDEO_TAGS=["TAG 1", "TAG 2"],
-    VIDEO_TOPIC = "TOPIC",
-    VIDEO_STAMP="STAMP")
+    VIDEO_TOPIC = "TOPIC")
 
 ModelTrainer.trainAVSdata(user_data, uuid.uuid4())
